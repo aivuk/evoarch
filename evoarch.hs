1c8f9b74fb44964479cbfa49f7e6a36e4981b27d
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-07-31 18:23:25 -0300

    Adding mkPhenMap function

--- evoarch.hs
+++ evoarch.hs
@@ -1,18 +1,18 @@
 import qualified Data.Vector.Unboxed as UV
 
-type Genotype = UV.Vector Int
-type Trait = Int
-type TraitMap = UV.Vector Int -> Genotype -> Trait
-type PhenMap = Genotype -> Phenotype
+type Genotype  = UV.Vector Int
+type Trait     = Int
+type TraitLoci = UV.Vector Int
+type TraitMap  = TraitLoci -> Genotype -> Trait
+type PhenMap   = Genotype -> Phenotype
 type Phenotype = UV.Vector Trait
 
 data Population = 
-    P { gens :: [Genotype]
+    P { gens  :: [Genotype]
       , phens :: [Phenotype] }
 
 createPop :: [Genotype] -> PhenMap -> Population
 createPop g pm = P g (map pm g) 
 
---mkPhenMap :: [TraitMap] -> PhenMap
---mkPhenMap = map (UV.sum $ UV.map (
-
+mkPhenMap :: [TraitMap] -> [TraitLoci] -> PhenMap
+mkPhenMap ts ti = UV.fromList.(zipWith ($) (zipWith ($) ts ti)).repeat
