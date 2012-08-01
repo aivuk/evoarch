9b152a24704a277fc5d0469f17d448630b4e8cf4
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-08-01 01:21:27 -0300

    first type of calctrait function. just sum genotype locus values

--- evoarch.hs
+++ evoarch.hs
@@ -1,4 +1,6 @@
 import qualified Data.Vector.Unboxed as UV
+import qualified System.Random.MWC as R
+import Control.Monad.Primitive (PrimMonad,PrimState)
 
 type Genotype  = UV.Vector Int
 type Trait     = Int
@@ -16,3 +18,9 @@ createPop g pm = P g (map pm g)
 
 mkPhenMap :: [TraitMap] -> [TraitLoci] -> PhenMap
 mkPhenMap ts ti = UV.fromList.(zipWith ($) (zipWith ($) ts ti)).repeat
+
+randomGenotype :: PrimMonad m => R.Gen (PrimState m) -> Int -> m Genotype
+randomGenotype g size = UV.replicateM size $ R.uniformR (-1,1) g
+
+sumTrait :: TraitLoci -> Genotype -> Trait
+sumTrait idx g = UV.sum $ UV.map (g UV.!) idx
